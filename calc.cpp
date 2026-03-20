1c56f46c6ef113606e6b23c8b7359f0d3a596140
Kirill <kirill504@ukr.net>
2026-03-20 11:14:33 +0200

    base calculator

---
+++ calc.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b, op;
+  double result;
+
+  cout << "Введіть перше число\n";
+  cin >> a;
+
+  cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n";
+  cin >> op;
+
+  cout << "Введіть друге число\n";
+  cin >> b;
+
+  switch(op) {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+  }
+
+  cout << "Результат = " << result;
+}
